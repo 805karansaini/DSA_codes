0444cbbd7833a6dcd15a71c1caa075559b01bdb6
805karansaini <805karansaini@gmail.com>
2022-04-10 17:07:02 +0530

    celling of a number : In sorted array return Highest index of number else return -1

---
+++ binarySearch/celling.py
@@ -0,0 +1,23 @@
+def celling(nums, target):
+    start = 0
+    last = len(nums) - 1
+    temp = -1
+
+    while start <= last:
+        mid = (start + (last - start) // 2)
+
+        if target > nums[mid]:
+            start = mid + 1
+        elif target < nums[mid]:
+            last = mid - 1
+        elif target == nums[mid]:
+            temp = mid
+            start = mid + 1
+    return temp;
+
+
+
+nums1 = [1,2,3,4,5,6,12,12,35,56,67]
+target1 = 68
+
+print(celling(nums1,target1))
