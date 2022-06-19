238c6a39b067cf198fa51e87b09f62c0baa6acd6
805karansaini <805karansaini@gmail.com>
2022-06-19 22:19:21 +0530

    optimized by adding flag. time taken reduced by 10 fold

--- LeetCode/1268. Search Suggestions System.py
+++ LeetCode/1268. Search Suggestions System.py
@@ -15,14 +15,17 @@ class Solution:
         N = len(products)
         n = len(searchWord)
         dic = defaultdict(list)
+        flag = True
         for i in range(n):
             c = 0
             temp = []
             j = 0
-            while c < 3 and j < N:
+            while c < 3 and j < N and flag: # due to flag time taken reduced by 10 fold
                 if products[j].startswith(searchWord[:i+1]):
                     temp.append(products[j])
                     c += 1
                 j += 1
+            if len(temp)==0:
+                flag = False # set flag as Flase and always append a empty list to res
             res.append(temp)
         return res
