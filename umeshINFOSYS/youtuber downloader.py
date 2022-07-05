93ac69d00d3e53c2c469ad69c659c5bfca312106
805karansaini <805karansaini@gmail.com>
2022-07-05 10:40:55 +0530

    Tried installing Pytube worked Great

--- umeshINFOSYS/youtuber downloader.py
+++ umeshINFOSYS/youtuber downloader.py
@@ -2,3 +2,5 @@ from pytube import YouTube
 YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
 yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
 yt.streams
+
+# youtube Downloader
