ab21848bb96ea4aaa39e6ada917b146473f06cfb
805karansaini <805karansaini@gmail.com>
2022-07-05 10:38:10 +0530

    LeetCode 128 longest Consecutive Sequence - Used HashSet O(n) solution

---
+++ LeetCode/128. Longest Consecutive Sequence.py
@@ -0,0 +1,21 @@
+# 128. Longest Consecutive Sequence
+# https://leetcode.com/problems/longest-consecutive-sequence/
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+# Input: nums = [0,3,7,2,5,8,4,6,0,1]
+# Output: 9
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        
+        for n in nums:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
