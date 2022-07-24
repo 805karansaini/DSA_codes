368c84a708767aff80dc4795cf3bb6726f0fceb4
805karansaini <805karansaini@gmail.com>
2022-07-24 18:56:35 +0530

    push

--- CodeChef/prime numbers SIEVE.py
+++ CodeChef/prime numbers SIEVE.py
@@ -3,25 +3,24 @@ def SieveOfEratosthenes(num):
 # boolean array
     p = 2
     while (p * p <= num):
- 
         # If prime[p] is not
         # changed, then it is a prime
         if (prime[p] == True):
- 
             # Updating all multiples of p
             for i in range(p * p, num+1, p):
                 prime[i] = False
         p += 1
- 
     
-    for p in range(2, num+1):
+    for p in range(10, num+1):
         if prime[p]:
+            temp.append(False)
+        else:
             temp.append(p)
     print(temp)
 # Driver code
 if __name__ == '__main__':
     temp = []
-    num = 100000
+    num = 89
     print("Following are the prime numbers smaller"),
     print("than or equal to", num)
     SieveOfEratosthenes(num)
