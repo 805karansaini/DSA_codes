e6e1213eb0967535af9aa3eb2d16226d73ef9e9c
805karansaini <805karansaini@gmail.com>
2022-08-04 21:55:55 +0530

    CF

---
+++ B_Permutation_Chain.py
@@ -0,0 +1,20 @@
+from collections import Counter, defaultdict
+import math
+def solve():
+    t = int(input())
+    while t:
+        t-=1
+        n = int(input())
+        nums = [ i+1 for i in range(n)]
+        print(n)
+        print(" ". join(map(str,nums)))
+
+        def swap(i,j):
+            nums[i], nums[j] = nums[j], nums[i]
+        for i in range(n-1):
+            swap(i,i+1)
+            print(" ". join(map(str,nums)))
+
+       
+    
+solve()
